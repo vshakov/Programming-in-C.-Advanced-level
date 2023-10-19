574546f5b4631276dafaed29564ad2781d4b651b
vshakov <86626877+vshakov@users.noreply.github.com>
2023-10-19 22:08:34 +0300

    Add files via upload

--- Homework#8/main.c
+++ Homework#8/main.c
@@ -6,58 +6,44 @@ double f1(double x) {
 }
 
 double f2(double x) {
-    return 3 * (x - 2) - 1;
+    return pow(x - 2, 3) - 1;
 }
 
 double f3(double x) {
-if (x != 0) {
-return 3 / x;
-} else {
-return 0; // Здесь можно вернуть любое другое значение, которое соответствует вашей логике
-}
+    return 3 / x;
 }
 
-double calculate_area(double lower_limit, double upper_limit) {
-    double area = 0;
-    double dx = 0.0001;  // Шаг интегрирования
+void find_intersections() {
+    double epsilon = 0.00001;  // Погрешность для определения пересечений
+    
+    // Перебор значений x в заданном диапазоне и проверка пересечений между функциями
+    for (double x = -10; x <= 10; x += epsilon) {
+        // Исключаем точку x = 0 при проверке с функцией f3(x)
+        if (x != 0) {
+            double y1 = f1(x);
+            double y2 = f2(x);
+            double y3 = f3(x);
 
-    for (double x = lower_limit; x < upper_limit; x += dx) {
-        double height = fmin(fmin(f1(x), f2(x)), f3(x));
-        area += height * dx;
+            // Проверка пересечения между функциями
+            if (fabs(y1 - y2) <= epsilon) {
+                printf("Точка пересечения f1(x) и f2(x): x = %lf\n", x);
+            }
+
+            if (fabs(y1 - y3) <= epsilon) {
+                printf("Точка пересечения f1(x) и f3(x): x = %lf\n", x);
+            }
+
+            if (fabs(y2 - y3) <= epsilon) {
+                printf("Точка пересечения f2(x) и f3(x): x = %lf\n", x);
+            }
+            
+
+        }
     }
-
-    return area;
 }
 
 int main() {
-    // Пересечения с осью x
-    double x_intersection_f1 = -3 / 0.6;
-    double x_intersection_f2 = 2;
-    double x_intersection_f3 = 0;
-
-    // Пересечения с осью y
-    double y_intersection_f1 = f1(0);
-    double y_intersection_f2 = f2(0);
-    double y_intersection_f3 = f3(0);
-
-    printf("Пересечения с осью x:\n");
-    printf("f1: x = %lf\n", x_intersection_f1);
-    printf("f2: x = %lf\n", x_intersection_f2);
-    printf("f3: x = %lf\n", x_intersection_f3);
-
-    printf("\nПересечения с осью y:\n");
-    printf("f1: y = %lf\n", y_intersection_f1);
-    printf("f2: y = %lf\n", y_intersection_f2);
-    printf("f3: y = %lf\n", y_intersection_f3);
-
-     // Определение пределов интегрирования
-    double lower_limit = fmax(fmax(x_intersection_f1, x_intersection_f2), x_intersection_f3);
-    double upper_limit = fmin(fmin(x_intersection_f1, x_intersection_f2), x_intersection_f3);
-
-    // Вычисление площади фигуры
-    double area = calculate_area(lower_limit, upper_limit);
-
-    printf("Площадь фигуры: %lf\n", area);
+    find_intersections();
 
     return 0;
 }
